8e4683b5d0fcfb705bf0298666ee13ced749e833
Todor Vlasev <t.vlasev@gmail.com>
2019-03-07 15:43:14 +0200

    creating update and delete routes, adding app router

--- src/index.js
+++ src/index.js
@@ -1,89 +1,14 @@
 const express = require('express')
+const userRouter = require('./routers/users')
+const taskRouter = require('./routers/tasks')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }catch(e){
-        res.status(400).send(e)
-    }
-
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-
-})
-
-app.get('/user/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try{
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch(e){
-        res.status(400).send()
-    }
-
-})
-
-app.post('/tasks', async (req, res) => {
-    const task =  new Task(req.body)
-
-    try{
-        await task.save()
-        res.status(201).send(task)
-    }catch(e){
-        res.status(400).send(e)
-    }
-
-})
-
-app.get('/tasks', async (req, res) =>{
-
-    try{
-        const tasks = await Task.find({})
-        res.send(tasks)
-    }catch(e){
-        res.status(500).send()
-    }
-
-})
-
-app.get('/task/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try{
-        const task = await Task.findById(_id)
-        if(!task){
-            res.status(404).send()
-        }
-
-        res.send(task)
-
-    }catch(e){
-        res.status(500).send()
-    }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
